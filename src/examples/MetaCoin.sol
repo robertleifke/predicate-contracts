96b05e9646cc8ca0b8d82c171b697a05fb43770b
Ayush Aggarwal <aagfye@gmail.com>
2024-10-14 17:08:38 -0700

    v1.0.1

---
+++ src/examples/MetaCoin.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+// Tells the Solidity compiler to compile only from v0.8.13 to v0.9.0
+pragma solidity =0.8.12;
+
+import {PredicateClient} from "../mixins/PredicateClient.sol";
+import {PredicateMessage} from "../interfaces/IPredicateClient.sol";
+
+// This is just a simple example of a coin-like contract.
+// It is not ERC20 compatible and cannot be expected to talk to other
+// coin/token contracts.
+
+contract MetaCoin is PredicateClient {
+    mapping(address => uint256) public balances;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    constructor(address owner, address serviceManager) {
+        balances[owner] = 10_000_000_000_000;
+        setServiceManager(serviceManager);
+        _transferOwnership(owner);
+    }
+
+    function sendCoin(address receiver, uint256 amount, PredicateMessage calldata predicateMessage) public {
+        bytes memory encodedSigAndArgs = abi.encodeWithSignature("_sendCoin(address,uint256)", receiver, amount);
+        require(_authorizeTransaction(predicateMessage, encodedSigAndArgs), "MetaCoin: unauthorized transaction");
+
+        // business logic function that is protected
+        _sendCoin(receiver, amount);
+    }
+
+    // business logic function that is protected
+    function _sendCoin(address receiver, uint256 amount) internal {
+        require(balances[msg.sender] >= amount, "MetaCoin: insufficient balance");
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+        emit Transfer(msg.sender, receiver, amount);
+    }
+
+    function getBalance(
+        address addr
+    ) public view returns (uint256) {
+        return balances[addr];
+    }
+}
---
+++ src/examples/MetaCoinWithRateLimit.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.12;
+
+import {Ownable} from "openzeppelin/access/Ownable.sol";
+import {RateLimiter} from "../mixins/RateLimiter.sol";
+
+contract MetaCoinWithRateLimit is RateLimiter {
+    mapping(address => uint256) public balances;
+    string public constant SYMBOL = "META";
+
+    error InsufficientBalance(address sender, uint256 requested, uint256 available);
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    constructor() {
+        balances[msg.sender] = 10_000_000_000_000;
+    }
+
+    function sendCoin(address receiver, uint256 amount) public {
+        if (!this.evaluateRateLimit(SYMBOL, amount)) {
+            revert("MetaCoinWithRateLimit: rate limit exceeded");
+        }
+        _sendCoin(receiver, amount);
+    }
+
+    function _sendCoin(address receiver, uint256 amount) internal {
+        if (balances[msg.sender] < amount) {
+            revert InsufficientBalance(msg.sender, amount, balances[msg.sender]);
+        }
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+        emit Transfer(msg.sender, receiver, amount);
+    }
+
+    function getBalance(
+        address addr
+    ) public view returns (uint256) {
+        return balances[addr];
+    }
+}
